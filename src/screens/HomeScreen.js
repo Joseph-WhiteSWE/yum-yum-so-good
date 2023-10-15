32c73f8269a3b6ba5552a3b964b4c3b78d03ab93
Joseph-WhiteSWE <white.joseph.jr@gmail.com>
2023-10-15 13:04:04 -0500

    installed reanimated and is working

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -4,8 +4,8 @@ import { SafeAreaView } from "react-native-safe-area-context"
 
 export default function HomeScreen() {
   return (
-    <SafeAreaView>
-      <Text>HomeScreen</Text>
+    <SafeAreaView className="flex-1 flex-col items-center justify-center">
+      <Text className="text-4xl">HomeScreen</Text>
     </SafeAreaView>
   )
 }
--- src/screens/WelcomeScreen.js
+++ src/screens/WelcomeScreen.js
@@ -1,26 +1,52 @@
+import Animated, { useSharedValue, withSpring } from "react-native-reanimated"
 import { View, Text, Image } from "react-native"
-import React from "react"
+import React, { useEffect } from "react"
 import { SafeAreaView } from "react-native-safe-area-context"
 import { StatusBar } from "expo-status-bar"
 import {
   widthPercentageToDP as wp,
   heightPercentageToDP as hp,
 } from "react-native-responsive-screen"
+import { useNavigation } from "@react-navigation/native"
 
 export default function WelcomeScreen() {
+  const ring1padding = useSharedValue(0)
+  const ring2padding = useSharedValue(0)
+
+  const navigation = useNavigation()
+
+  useEffect(() => {
+    ring1padding.value = 0
+    ring2padding.value = 0
+    setTimeout(
+      () => (ring1padding.value = withSpring(ring1padding.value + hp(5))),
+      100
+    )
+    setTimeout(
+      () => (ring2padding.value = withSpring(ring2padding.value + hp(5.5))),
+      300
+    )
+    setTimeout(() => navigation.navigate("Home"), 2500)
+  }, [])
   return (
     <SafeAreaView className="flex-1 justify-center items-center space-y-10 bg-amber-500">
       <StatusBar style="light" />
 
       {/* center image */}
-      <View className="bg-white/20 rounded-full" style={{ padding: hp(5.5) }}>
-        <View className="bg-white/20 rounded-full" style={{ padding: hp(5) }}>
+      <Animated.View
+        className="bg-white/20 rounded-full"
+        style={{ padding: ring1padding }}
+      >
+        <Animated.View
+          className="bg-white/20 rounded-full"
+          style={{ padding: ring2padding }}
+        >
           <Image
             source={require("../../assets/img/welcome.png")}
             style={{ width: hp(20), height: hp(20) }}
           />
-        </View>
-      </View>
+        </Animated.View>
+      </Animated.View>
 
       {/* title and punch line */}
       <View className="flex items-center spacey-y-2">
@@ -31,7 +57,7 @@ export default function WelcomeScreen() {
           Hungry?
         </Text>
         <Text
-          style={{ fontSize: hp(2) }}
+          style={{ fontSize: hp(4) }}
           className="font-medium text-white tracking-widest"
         >
           Tap In!
