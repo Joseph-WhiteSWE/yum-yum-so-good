fef494edae635102b4fbf4bd41b9543962e604bf
Joseph-WhiteSWE <white.joseph.jr@gmail.com>
2023-10-15 13:27:50 -0500

    created avatar in HomeScreen

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,11 +1,29 @@
-import { View, Text } from "react-native"
+import { View, Text, ScrollView, Image } from "react-native"
 import React from "react"
 import { SafeAreaView } from "react-native-safe-area-context"
+import { StatusBar } from "expo-status-bar"
+import {
+  widthPercentageToDP as wp,
+  heightPercentageToDP as hp,
+} from "react-native-responsive-screen"
 
 export default function HomeScreen() {
   return (
-    <SafeAreaView className="flex-1 flex-col items-center justify-center">
-      <Text className="text-4xl">HomeScreen</Text>
+    <SafeAreaView className="flex-1 bg-white">
+      <StatusBar style="dark" />
+      <ScrollView
+        showsVerticalScrollIndicatior={false}
+        contentComntainerStyle={{ paddingBottom: 50 }}
+        className="space-y-6 pt-14"
+      >
+        {/* avatar and bell icon */}
+        <View className="mx-4 flex-row justify-between items-center mb-2">
+          <Image
+            source={require("../../assets/img/avatar.png")}
+            style={{ height: hp(5.5), width: hp(5.5) }}
+          />
+        </View>
+      </ScrollView>
     </SafeAreaView>
   )
 }
