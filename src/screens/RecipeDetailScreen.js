4b77f63e33fe23c2b63a7ed56a9e7123a16a89ff
joseph <white.joseph.jr@gmail.com>
2023-10-30 16:17:45 -0500

    added a goBack function and heart functionality

--- src/screens/RecipeDetailScreen.js
+++ src/screens/RecipeDetailScreen.js
@@ -1,14 +1,19 @@
-import { View, Text, ScrollView } from "react-native"
-import React from "react"
+import { View, Text, ScrollView, TouchableOpacity } from "react-native"
+import React, { useState } from "react"
 import { StatusBar } from "expo-status-bar"
 import { CachedImage } from "../helpers/image"
 import {
   widthPercentageToDP as wp,
   heightPercentageToDP as hp,
 } from "react-native-responsive-screen"
+import { ChevronLeftIcon } from "react-native-heroicons/outline"
+import { HeartIcon } from "react-native-heroicons/solid"
+import { useNavigation } from "@react-navigation/native"
 
 export default function RecipeDetailScreen(props) {
   const item = props.route.params
+  const [isFavorite, setIsFavorite] = useState(false)
+  const navigation = useNavigation()
   return (
     <ScrollView
       className="bg-gray flex-1"
@@ -30,6 +35,25 @@ export default function RecipeDetailScreen(props) {
             marginTop: 4,
           }}
         />
+      </View>
+      {/* back button */}
+      <View className="w-full absolute flex-row justify-between items-center pt-14">
+        <TouchableOpacity
+          onPress={() => navigation.goBack()}
+          className="p-2 rounded-full ml-5 bg-white"
+        >
+          <ChevronLeftIcon size={hp(3.5)} strokeWidth={4.5} color="#fbbf24" />
+        </TouchableOpacity>
+        <TouchableOpacity
+          onPress={() => setIsFavorite(!isFavorite)}
+          className="p-2 rounded-full mr-5 bg-white"
+        >
+          <HeartIcon
+            size={hp(3.5)}
+            strokeWidth={4.5}
+            color={isFavorite ? "red" : "gray"}
+          />
+        </TouchableOpacity>
       </View>
     </ScrollView>
   )
