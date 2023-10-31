e52e230af99f6f9608fe775cd25c849630ea3d1e
joseph <white.joseph.jr@gmail.com>
2023-10-30 19:10:44 -0500

    imported data from database

--- src/screens/RecipeDetailScreen.js
+++ src/screens/RecipeDetailScreen.js
@@ -1,5 +1,5 @@
 import { View, Text, ScrollView, TouchableOpacity } from "react-native"
-import React, { useState } from "react"
+import React, { useEffect, useState } from "react"
 import { StatusBar } from "expo-status-bar"
 import { CachedImage } from "../helpers/image"
 import {
@@ -9,11 +9,34 @@ import {
 import { ChevronLeftIcon } from "react-native-heroicons/outline"
 import { HeartIcon } from "react-native-heroicons/solid"
 import { useNavigation } from "@react-navigation/native"
+import axios from "axios"
+import Loading from "../components.js/loading"
 
 export default function RecipeDetailScreen(props) {
   const item = props.route.params
   const [isFavorite, setIsFavorite] = useState(false)
   const navigation = useNavigation()
+  const [meal, setMeal] = useState(null)
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    getMealData(item.idMeal)
+  }, [])
+
+  const getMealData = async id => {
+    try {
+      const response = await axios.get(
+        `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`
+      )
+      console.log(`got meal data: `, response.data)
+      if (response && response.data) {
+        setMeal(response.data.meals[0])
+        setLoading(false)
+      }
+    } catch (err) {
+      console.log("error", err.message)
+    }
+  }
   return (
     <ScrollView
       className="bg-gray flex-1"
@@ -55,6 +78,26 @@ export default function RecipeDetailScreen(props) {
           />
         </TouchableOpacity>
       </View>
+      {/* meal description */}
+      {loading ? (
+        <Loading size="large" className="mt-16" />
+      ) : (
+        <View className="px-4 flex justify-between space-y-4 pt-8">
+          {/* name and area */}
+          <Text
+            style={{ fontSize: hp(3) }}
+            className="font-bold flex-1 text-neutral-700"
+          >
+            {meal?.strMeal}
+          </Text>
+          <Text
+            style={{ fontSize: hp(2) }}
+            className="font-medium flex-1 text-neutral-500"
+          >
+            {meal?.strArea}
+          </Text>
+        </View>
+      )}
     </ScrollView>
   )
 }
